048e651d6fddc657e5b71a0bf6c069599cf045ca
Matias Leonel Ramirez <matt.neetz.dev.@gmail.com>
2023-08-27 20:07:16 -0300

    Initial commit

---
+++ actividad_integradora/app/Views/front/footer_view.php
@@ -0,0 +1,16 @@
+<footer class="footer text-center">
+	<div class="footer-content ">
+		<div class="social-icons">
+			<a href="https://www.facebook.com/" class="social-icon"><img src="assets/img/imagenfacebook.png" alt="Facebook" width="35" height="10" class="img-fluid"></a>
+
+			<a href="https://www.instagram.com/" class="social-icon"><img src="assets/img/imageninstagram.png" alt="Instagram" width="35" height="10" class="img-fluid"></a>
+
+			<a href="https://twitter.com/home" class="social-icon"><img src="assets/img/imagentwitter.png"  alt="Twitter" width="35" height="10" class="img-fluid"></a>
+
+		</div>
+		<p class="bg-succes p-2 text-dark">Derechos de autor © 2023 | Talentos Digitales </p>
+	</div>
+</footer>
+<script src="assets/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
---
+++ actividad_integradora/app/Views/front/head_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>tp2</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" type="text/css" href="assets/css/s/style.css">
+  </head>
+  <header>
+  	<section class="">
+  	<h1 class="text-light text-center ">Bienvenidos!</h1>
+  </section>
+
+  </header>
+  
+
+  <body id="cuerpo">
+  	
+
+ 
